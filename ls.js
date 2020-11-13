6f3c46af996dd9e598a6ce4812162ed0d9fa6894
Edwin Rodriguez <RodriguezEdwin1324@gmail.com>
2020-11-13 14:34:33 -0500

    customized spacing on ls command

--- ls.js
+++ ls.js
@@ -4,7 +4,16 @@ module.exports = (string) => {
   fs.readdir(__dirname, (err, data) => {
     if (string === "ls") {
       //   process.stdout.write("\n");
-      process.stdout.write(data.map((element) => `   ${element}`).join(""));
+      process.stdout.write(
+        data
+          .map((element, index) => {
+            if (index !== 0) {
+              return `   ${element}`;
+            }
+            return element;
+          })
+          .join("")
+      );
       process.stdout.write("\nPrompt > ");
     }
   });
